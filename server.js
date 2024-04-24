1ce1824bba964357a3d41e64a53ad2e3fe62d818
dongmay <sdh4202@naver.com>
2024-04-24 15:18:54 +0900

    Feat: cors설정변경

--- server.js
+++ server.js
@@ -12,38 +12,38 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.json());
 
-// // CORS 설정
-// const corsOptions = {
-//   origin: [
-//     "https://dongmay98.github.io",
-//     "https://dongmay98.github.io/GoogleForm_client-TS",
-//     "https://port-0-googleform-server-85phb42bluv1sf6v.sel5.cloudtype.app",
-//   ],
-//   methods: ["GET", "POST"],
-//   credentials: true,
-//   optionsSuccessStatus: 200,
-// };
-// app.use(cors(corsOptions));
+// CORS 설정
+const corsOptions = {
+  origin: [
+    "https://dongmay98.github.io",
+    "https://dongmay98.github.io/GoogleForm_client-TS",
+    "https://port-0-googleform-server-85phb42bluv1sf6v.sel5.cloudtype.app",
+  ],
+  methods: ["GET", "POST"],
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
-// // 응답 헤더에 Access-Control-Allow-Credentials 설정
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Credentials", true);
-//   next();
-// });
-app.use(function (req, res, next) {
+// 응답 헤더에 Access-Control-Allow-Credentials 설정
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
-  if ("OPTIONS" == req.method) {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
+  next();
 });
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header("Access-Control-Allow-Origin", req.headers.origin);
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+//   );
+//   if ("OPTIONS" == req.method) {
+//     res.sendStatus(200);
+//   } else {
+//     next();
+//   }
+// });
 
 app.get("/", (req, res) => {
   res.send("hello");
